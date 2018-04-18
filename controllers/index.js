bf4bde74abee4acc5a38cf63fe449668b8d1a270
Sangeetha <sangeetha.detne@gmail.com>
2018-04-17 22:17:54 -0500

    created admin pages and chaged some functionality

--- controllers/index.js
+++ controllers/index.js
@@ -11,6 +11,7 @@ app.get('/',(req,res) =>{
     
             res.render('./home.ejs');
 });
+
 app.post('/',(req,res) =>{
     var username;
     console.log("Post data")
@@ -42,40 +43,46 @@ app.get('/About',(req,res) =>{
 app.get('/menu',(req,res) =>{
     // 
             res.render('./menu.ejs');
-});app.get('/profile',(req,res) =>{
+});
+app.get('/profile',(req,res) =>{
     // 
             res.render('./profile.ejs');
-});app.get('/cart',(req,res) =>{
+});
+app.get('/cart',(req,res) =>{
     // 
             res.render('./cart.ejs');
-});app.get('/header',(req,res) =>{
+});
+app.get('/header',(req,res) =>{
     // 
             res.render('./header.ejs');
 });
-// app.get('/studentrooms',(req,res) =>{
-//     
-//             res.render('./studentrooms.ejs');
-// });
 
-// app.get('/studenthistory',(req,res) =>{
-//     
-//             res.render('./studenthistory.ejs');
-// });
-// app.get('/studentprofile',(req,res) =>{
-//     
-//             res.render('./studentprofile.ejs');
-// });
-// app.get('/logout',(req,res) =>{
-//     
-//             res.render('./logout.ejs');
-// });
-// app.get('/aboutus',(req,res) =>{
-//             res.render('./aboutus.ejs');
-// });
+//sangeetha
 
-// app.get('/feedback',(req,res) =>{
-//     res.render('./feedback.ejs');
-// });
+app.get('/addremoveuser',(req,res) =>{
+   
+            res.render('./Admin_AddRemoveUser.ejs');
+});
+app.get('/adminprofile',(req,res) =>{
+    
+             res.render('./Admin_Profile.ejs');
+ });
+ app.get('/specialoffer',(req,res) =>{
+    
+             res.render('./Admin_SpecialOffer.ejs');
+ });
+ app.get('/adminupdatecategory',(req,res) =>{
+    
+             res.render('./Admin_UpdateCategory.ejs');
+ });
+ app.get('/adminupdatemenu',(req,res) =>{
+    
+             res.render('./Admin_UpdateMenu.ejs');
+ });
+
+
+
+
 
 
 
